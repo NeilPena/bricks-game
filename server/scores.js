d3f2d04bfa94bb7aeaca4f140102ef031fc5e846
Neil Pena <ne636020@ucf.edu>
2026-08-09 19:29:20 -0400

    Fix scores.js file

--- server/scores.js
+++ server/scores.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Score = require('../models/Score');
+const Score = require('../server/Score');
 
 // GET /api/scores - top 10 high scores
 router.get('/', async (req, res) => {
